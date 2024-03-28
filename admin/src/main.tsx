636f4133267a7e7c25e5cf66549623902a46dc23
Moryoka <mariovlaviano2005@yahoo.com>
2024-03-28 10:10:19 +0200

    Add bootstrap to project

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './assets/css/styles.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
