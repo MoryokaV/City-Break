da9bcdf6064a01b8de640dba2409cad78c30b407
Moryoka <mariovlaviano2005@yahoo.com>
2024-03-30 12:07:20 +0200

    Styles not found page

--- admin/src/pages/404.tsx
+++ admin/src/pages/404.tsx
@@ -1,12 +1,15 @@
 import { useRouteError } from "react-router-dom";
+import styles from "../assets/css/404.module.css";
+import NotFoundSVG from "../assets/illustrations/not_found.svg";
 
 export default function ErrorPage() {
   const error = useRouteError();
 
   return (
-    <div>
+    <div className={`d-flex flex-column align-items-center justify-content-center w-100 h-100 p-4 ${styles.fill_root}`}>
+      <img src={NotFoundSVG} alt="Not found illustration" className={styles.icon} />
       <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <p className={styles.info}>Sorry, an unexpected error has occurred.</p>
       <p>
         <i>
           {(error as Error)?.message || (error as { statusText?: string })?.statusText}
