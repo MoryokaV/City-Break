66c25f25e7350d30fa4f57a5593cfbca7cbe26f4
Moryoka <mariovlaviano2005@yahoo.com>
2024-03-30 22:45:17 +0200

    Add tour table in dashboard

---
+++ admin/src/components/Tables/TourTable.tsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import DashboardCard from "../DashboardCard";
+import { IoCreateOutline, IoRemoveCircleOutline } from "react-icons/io5";
+import { Tour } from "../../models/TourModel";
+import { LoadingSpinner } from "../LoadingSpinner";
+
+export const TourTable = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [tours, setTours] = useState<Array<Tour>>([]);
+
+  useEffect(() => {
+    fetch(
+      "https://cb.bjbraila.ro/api/fetchTours?city_id=5331e6c5772d490dabeb124502b96882",
+    )
+      .then(response => response.json())
+      .then(data => {
+        setTours(data);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <DashboardCard title="Tours" records={tours.length}>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Stages</th>
+            <th>External link</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {isLoading ? (
+            <tr>
+              <td colSpan={100} className="my-auto text-center">
+                <LoadingSpinner />
+              </td>
+            </tr>
+          ) : (
+            <>
+              {tours.map((tour, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{tour._id}</td>
+                    <td>{tour.name}</td>
+                    <td>{tour.stages.map(stage => stage.text).join(" - ")}</td>
+                    <td>
+                      <a href={tour.external_link} target="_blank">
+                        {tour.external_link}
+                      </a>
+                    </td>
+                    <td id={tour._id}>
+                      <div className="group">
+                        <button
+                          className="btn-icon action-edit-tour"
+                          data-bs-toggle="modal"
+                          data-bs-target="#tour-modal"
+                        >
+                          <IoCreateOutline className="edit-icon" />
+                        </button>
+                        <button className="btn-icon action-delete-tour">
+                          <IoRemoveCircleOutline className="edit-icon" />
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </>
+          )}
+        </tbody>
+      </table>
+    </DashboardCard>
+  );
+};
--- admin/src/pages/dashboard.tsx
+++ admin/src/pages/dashboard.tsx
@@ -1,10 +1,12 @@
 import { SightTable } from "../components/Tables/SightTable";
+import { TourTable } from "../components/Tables/TourTable";
 
 export default function Dashboard() {
   return (
     <div className="container-fluid p-3">
       <div className="row gy-3 gx-0">
-        <SightTable/>
+        <SightTable />
+        <TourTable />
       </div>
     </div>
   );
