6c3c2a6e0b18f3fb52860a6114fba302fc57828c
Moryoka <mariovlaviano2005@yahoo.com>
2024-04-17 15:35:09 +0300

    Test submission cancel

--- admin/src/components/Forms/InsertSightForm.tsx
+++ admin/src/components/Forms/InsertSightForm.tsx
@@ -50,6 +50,7 @@ export const InsertSightForm: React.FC<Props> = ({
     }).then(response => {
       if (response.status === 413) {
         alert("Files size should be less than 15MB");
+        return;
       }
     });
 
